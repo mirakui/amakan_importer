6065f3588bafe58aa21d359500b58d7ef02f7adb
Issei Naruta <naruta@cookpad.com>
2017-09-20 16:00:15 +0900

    output

--- fetch_kindle_list.rb
+++ fetch_kindle_list.rb
@@ -1,6 +1,9 @@
 require 'chrome_remote'
+require 'fileutils'
 
 class CLI
+  OUTPUT_DIR = File.expand_path('../output/', __FILE__)
+
   def cmd_init
     url = 'https://www.amazon.co.jp/mn/dcw/myx.html/ref=kinw_myk_redirect'
     chrome.send_cmd 'Page.navigate', url: url
@@ -21,12 +24,18 @@ Opened #{url}
 
     result_value = out['result']['value']
     length = result_value['length'].to_i
-    asins = (0...length).map do |i|
-      result_value[i.to_s].sub(/^contentTabList_/, '')
-    end
-    p asins
+    puts "extracted #{length} items"
 
-    puts "extracted #{asins.length} items"
+    FileUtils.mkdir_p OUTPUT_DIR unless File.directory?(OUTPUT_DIR)
+    fname = "asins-#{Time.now.strftime('%Y%m%d-%H%M')}"
+    path = File.join(OUTPUT_DIR, fname)
+
+    puts "writing to #{path}"
+    open(path, 'w+') do |f|
+      asins = (0...length).each do |i|
+        f.puts result_value[i.to_s].sub(/^contentTabList_/, '')
+      end
+    end
   end
 
   def cmd_usage
